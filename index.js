e2c875523b62a375824642c8bb1f6e1319a3f662
richard200 <richmn200@gmail.com>
2022-11-21 18:02:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// let rem = document.getElementsByTagName("div")[0];
+// let rem2 = div.querySelector("main");
+// div.removeChild(rem2);
+
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Richard is the champion";
